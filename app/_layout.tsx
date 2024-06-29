9d11c51cfc90078c0179bcdb0efb144f1c6ffc23
dsoffiantini <dsoffiantini@gmail.com>
2024-06-29 12:28:46 -0600

    feat: :sparkles: auth app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,18 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, router, useFocusEffect } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { auth } from "./firebase";
+import { SafeAreaView, Text } from "react-native";
+import Login from "./login";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,8 +20,9 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const [user, setUser] = useState(auth.currentUser);
 
   useEffect(() => {
     if (loaded) {
@@ -22,16 +30,48 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!user) { 
+        router.replace('/login');
+      } else {
+        router.replace('/');
+      }
+    }, [user])
+  );
+
+  // useEffect(() => {
+  //   auth.currentUser?.getIdToken().then((token) => console.log(token));
+  // }, []);
+
   if (!loaded) {
     return null;
   }
 
+  console.log(user);
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaView style={{ marginTop: 30, flex: 1 }}>
+        {user ? (
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        ) : (
+          <Stack>
+            <Stack.Screen name="login" options={{ headerShown: false }} />
+            <Stack.Screen name="signup" options={{ headerShown: false }} />
+          </Stack>
+        )}
+      </SafeAreaView>
     </ThemeProvider>
   );
 }

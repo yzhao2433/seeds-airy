8343eea4013980510fd9f14eb137d07be98cda1c
yzhao2433 <yinglin991@gmail.com>
2024-06-30 15:06:34 -0400

    Added Firebase config

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,18 +1,21 @@
 // Import the functions you need from the SDKs you need
+//import AsyncStorage from "@react-native-async-storage/async-storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+// used npm install --save https://github.com/redux-offline/redux-offline#native
+// from https://github.com/redux-offline/redux-offline/issues/436
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBcGa5X5xr6AMMGrGhzwibGvhtbDUtylt4",
+  authDomain: "airyseeds.firebaseapp.com",
+  projectId: "airyseeds",
+  storageBucket: "airyseeds.appspot.com",
+  messagingSenderId: "892588792705",
+  appId: "1:892588792705:web:f144386199fe8698c36680",
 };
 
 // Initialize Firebase

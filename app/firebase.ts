1353c17c40395408576b1b5d636552c79f392a5e
yzhao2433 <yinglin991@gmail.com>
2024-07-10 01:06:34 -0400

    Added code to link auth to Firestore
    When a user signs up, the user.uid should be used to create a new document in the Firestore "user" collection.

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,12 +1,19 @@
 // Import the functions you need from the SDKs you need
-//import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { initializeApp } from "firebase/app";
-import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+// for issues related to async storage use one of the two approaches
 // used npm install --save https://github.com/redux-offline/redux-offline#native
 // from https://github.com/redux-offline/redux-offline/issues/436
+// or npm i @react-native-async-storage/async-storage
+
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+// should be fixed via the "@firebase/auth": ["./node_modules/@firebase/auth/dist/index.rn.d.ts"]
+// line in tsconfig.json
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+//// for set up, if you haven't already, run npm install -- save firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,18 +1,27 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Share, Text, TextInput, View } from "react-native";
 import { AuthError, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { auth } from "./firebase";
+import { app } from "./firebase";
 import { Controller, useForm } from "react-hook-form";
 import { Link } from "expo-router";
 
 interface FormValues {
   email: string;
   password: string;
+  nickname: string;
+  listHobbies: string;
 }
 
 const defaultValues: FormValues = {
   email: "",
   password: "",
+  nickname: "",
+  listHobbies: "",
 };
+
+// Get reference to collection
+const db = getFirestore(app);
 
 function SignUp() {
   const { control, formState, handleSubmit, setError } = useForm<FormValues>({
@@ -22,16 +31,35 @@ function SignUp() {
   const onSubmit = async (values: FormValues) => {
     console.log(values);
     try {
+      // add user to auth
       const user = await createUserWithEmailAndPassword(
         auth,
         values.email,
         values.password
       );
-      console.log(user);
-    } catch (e) {
-      const error = e as AuthError;
 
-      console.log(e);
+      // create a new user document to add a new user to the "user" collection
+      const newUser = {
+        uid: user.user.uid,
+        messageLeft: 10,
+        // need to edit
+        avatar: "blank",
+        hobbies: values.listHobbies,
+        moods: [],
+        thoughts: [],
+        messagesReceived: [],
+      };
+      const newUserRef = doc(db, "user", user.user.uid);
+      await setDoc(newUserRef, newUser);
+
+      console.log("User added with ID: ", user.user.uid);
+    } catch (error) {
+      console.error("Error adding user: ", error);
+      // if (error instanceof AuthError) {
+      //   setError("email", { message: error.message });
+      // } else {
+      //   setError("general", { message: "An unexpected error occurred" });
+      // }
     }
   };
 
@@ -60,6 +88,26 @@ function SignUp() {
         control={control}
         name="password"
         rules={{ required: true, minLength: 6 }}
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+      />
+
+      <Text>Preferred Nickname</Text>
+      <Controller
+        control={control}
+        name="nickname"
+        rules={{ required: true, minLength: 2 }}
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+      />
+
+      <Text>Hobbies</Text>
+      <Controller
+        control={control}
+        name="listHobbies"
+        rules={{ required: true, minLength: 2 }}
         render={({ field: { onBlur, onChange, value } }) => (
           <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
         )}
